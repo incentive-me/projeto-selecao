3878da4255834a4e30a9079e1cf23ef244f32f8d
ardotheedu <eduardosalima3@gmail.com>
2021-05-26 11:46:51 -0300

    feat: autenticação do usuario usando API do github.

---
+++ src/index.js
@@ -0,0 +1,53 @@
+const express = require('express');
+const app = express();
+
+app.use(express.static("public"))
+
+// habilitar o uso do req.body na nossa aplicação
+app.use(express.urlencoded({ extended: true }))
+
+// utilizando template engine
+const nunjucks = require("nunjucks")
+nunjucks.configure("src/views", {
+    express: app,
+    noCache: true
+})
+
+var access_token = "";
+
+app.get("/", (req, res) => {
+  return res.render("index.html")
+})
+
+const port = process.env.PORT || 3000;
+app.listen(port , () => console.log('App listening on port ' + port));
+
+// Import the axios library, to make HTTP requests
+const axios = require('axios')
+// This is the client ID and client secret that you obtained
+// while registering on github app
+const clientID = '373c622a3395445d77e9'
+const clientSecret = '597ae04f9326dc3baa5113af4e82387c78cecbe4'
+
+// Declare the callback route
+app.get('/github/callback', (req, res) => {
+
+  // The req.query object has the query params that were sent to this route.
+  const requestToken = req.query.code
+  
+  axios({
+    method: 'post',
+    url: `https://github.com/login/oauth/access_token?client_id=${clientID}&client_secret=${clientSecret}&code=${requestToken}`,
+    // Set the content type header, so that we get the response in JSON
+    headers: {
+         accept: 'application/json'
+    }
+  }).then((response) => {
+    access_token = response.data.access_token
+    res.redirect('/success');
+  })
+})
+
+app.get('/success', async function(req, res) {
+  res.render('success.html');
+});
