b901f32a330c07787fba384dbbd57cd46eaad600
Nathan Firmo <nathanssfirmo@gmail.com>
2021-05-27 13:12:16 -0300

    Adicionando html e api/index

---
+++ README.md
@@ -0,0 +1,1 @@
+[https://github.com/login/oauth/authorize?response_type=code&client_id=446001807a7f80019e37&scope=repo]
---
+++ api/index.js
@@ -0,0 +1,27 @@
+const nodemailer = require('nodemailer')
+const transporter = nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+    user: 'dev.autosender@gmail.com',
+    pass: process.env.PASS
+  }
+})
+
+async function sender(req, res) {
+  const sender = `${req.body.name} <${req.body.email}>`
+  const message = `${req.body.message}`
+
+  if (message) {
+    await transporter.sendMail({
+      from: `${sender}`,
+      text: `${message}`,
+      subject: 'Formulário de contato',
+      to: 'nathanssfirmo@gmail.com',
+      replyTo: `${sender}`
+    })
+    console.log('Email sent')
+  }
+  res.redirect('https://projeto-selecao.vercel.app/tanks')
+}
+
+module.exports = sender
