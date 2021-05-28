f02df498229df3244f695fe92ad1cd14170a676b
Nathan Firmo <nathanssfirmo@gmail.com>
2021-05-27 21:13:14 -0300

    Estruturando o HTML e os estilos

---
+++ public/javascript/main.js
@@ -0,0 +1,31 @@
+function quemSomos() {
+document.getElementById("index").style.display = "flex"
+document.getElementById("copyCode").style.display = "none"
+document.getElementById("copyCss").style.display = "none"
+document.getElementById("howItWorks").style.display = "none"
+}
+
+function importarHTML(){
+    document.getElementById("index").style.display = "none"
+    document.getElementById("copyCode").style.display = "block"
+    document.getElementById("copyCss").style.display = "none"
+    document.getElementById("howItWorks").style.display = "none"
+}
+
+function importarCSS(){
+    document.getElementById("index").style.display = "none"
+    document.getElementById("copyCode").style.display = "none"
+    document.getElementById("copyCss").style.display = "block"
+    document.getElementById("howItWorks").style.display = "none"
+}
+
+function sobreANossaAPI(){
+    document.getElementById("index").style.display = "none"
+    document.getElementById("copyCode").style.display = "none"
+    document.getElementById("copyCss").style.display = "none"
+    document.getElementById("howItWorks").style.display = "block"
+}
+
+function copyHTML() {
+
+}
