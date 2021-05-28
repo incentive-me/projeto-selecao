71ace3ec51b0f0dc94ba31702561620ecab0e989
Rafael Carvalho <rafaelvfcarvalho@gmail.com>
2021-05-28 14:43:19 -0300

    fetch starred projects

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,102 @@
+import axios from 'axios';
+import { GetServerSideProps } from 'next';
+import { getSession, useSession } from 'next-auth/client'
 import Head from 'next/head'
+import prisma from '../lib/prisma';
 
-export default function Home() {
+type Project = {
+    id: any
+    name: string
+    url: string
+    description: string
+}
+
+interface HomeProps {
+    projects: Project[]
+}
+
+export default function Home({ projects }: HomeProps) {
+    const [session] = useSession();
 
     return (
         <>
             <Head>
                 <title>Labeled Github Stars</title>
             </Head>
-            <div>Hello World!</div>
+            {projects.map(project => (
+                <article key={project.id}>
+                    <h2>{project.id}</h2>
+                    <p>{project.name}</p>
+                    <p>{project.url}</p>
+                    <p>{project.description}</p>
+                </article>
+            ))}
+            {/*<pre>
+                {JSON.stringify(projects, null, 2)}
+            </pre> */}
         </>
     )
+}
+
+type DatabaseProject = {
+    userId: number
+    id: number
+}
+
+export const getServerSideProps: GetServerSideProps = async (ctx) => {
+    const session = await getSession({ ctx })
+
+    const user = await prisma.user.findFirst({
+        where: {
+            email: session.user.email,
+        },
+    })
+
+    if (!session || !user) {
+        return {
+            redirect: {
+                destination: '/login',
+                permanent: false,
+            },
+        }
+    }
+
+    let fetchedProjects = [];
+
+    if (session.accessToken) {
+        fetchedProjects = await axios.get('https://api.github.com/user/starred', {
+            headers: {
+                Authorization: `token ${session.accessToken}`,
+                Accept: 'application/vnd.github.v3+json'
+            }
+        }).then(response => response.data)
+    }
+
+    let projects: Project[] = [];
+    let databaseProjects: DatabaseProject[] = [];
+
+    fetchedProjects.forEach(project => {
+        projects.push({
+            id: project.id,
+            name: 'project.full_name',
+            url: project.html_url,
+            description: project.description,    
+        })
+
+        databaseProjects.push({
+            userId: user.id,
+            id: project.id
+        })
+    })
+
+    await prisma.project.createMany({
+        skipDuplicates: true,
+        data: databaseProjects,
+    })
+
+    return {
+        props: {
+            projects
+        }
+    }
 }
