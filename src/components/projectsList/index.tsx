5fb9da80ac886a0ebc9a08a87988bf42e52f7af2
Rafael Carvalho <rafaelvfcarvalho@gmail.com>
2021-05-30 12:22:07 -0300

    filter projects by label

--- src/components/projectsList/index.tsx
+++ src/components/projectsList/index.tsx
@@ -1,6 +1,6 @@
-import { Dispatch, KeyboardEvent, SetStateAction, useContext, useEffect, useRef, useState } from "react"
+import { KeyboardEvent, useContext, useEffect, useRef, useState } from "react"
 import { Project, ProjectsContext } from "../../contexts/projectsContext"
-import { ProjectListContainer } from "./styles"
+import { ProjectListContainer, SearchContainer } from "./styles"
 
 import ProjectCard from '../ProjectCard'
 
@@ -10,16 +10,47 @@ type ProjectsListProps = {
 
 export default function Projects({ initialProjects }: ProjectsListProps) {
     const { setProjects, projects } = useContext(ProjectsContext);
+    const [filteredProjects, setFilteredProjects] = useState<Project[]>()
+    const searchInputRef = useRef<HTMLInputElement>();
 
     useEffect(() => {
         setProjects(initialProjects)
+        setFilteredProjects(initialProjects)
+
+        searchInputRef.current.focus()
     }, [])
+
+    function handleSearch(event: KeyboardEvent<HTMLInputElement>) {
+        if (searchInputRef.current.value === '') {
+            showAllProjects();
+            return;
+        }
+        const filtered = projects.filter(project => {
+            return project.labels.some(item => item.includes(searchInputRef.current.value))
+        })
+        setFilteredProjects(filtered)
+    }
+
+    function showAllProjects() {
+        searchInputRef.current.value = ''
+        setFilteredProjects(projects)
+    }
 
     return (
         <div>
-            <h2>Starred Projects</h2>
             <ProjectListContainer>
-                {projects?.map(project => (
+                <h2>Starred Projects</h2>
+                <SearchContainer>
+                    <input
+                        name="search"
+                        placeholder="🔎 Search by Label"
+                        type="text"
+                        ref={searchInputRef}
+                        onKeyUp={handleSearch}
+                    />
+                    <button onClick={showAllProjects}>≣ Show all</button>
+                </SearchContainer>
+                {filteredProjects?.map(project => (
                     <ProjectCard project={project} key={project.id} />
                 ))}
             </ProjectListContainer>
--- src/components/projectsList/styles.ts
+++ src/components/projectsList/styles.ts
@@ -3,12 +3,39 @@ import styled from 'styled-components'
 export const ProjectListContainer = styled.section`
     display: flex;
     flex-wrap: wrap;
+    margin-top: 2rem;
+
+    h2 {
+        margin-bottom: 0;
+        padding: 0 1rem;
+    }
 
     @media(min-width: 768px) {
-        max-width: 700px;
+        width: 700px;
     }
 
     @media(min-width: 1024px) {
-        max-width: 1000px;
+        width: 1000px;
+    }
+`
+export const SearchContainer = styled.div`
+    width: 100%;
+    padding: 1rem;
+
+    input {
+        width: 100%;
+        max-width: 300px;
+        padding: .5rem;
+
+        border: 2px solid #c1c1c1;
+        border-radius: .5rem;
+    }
+    button {
+        margin-left: 4px;
+        padding: .5rem;
+
+        border: 2px solid #c1c1c1;
+        border-radius: .5rem;
+        cursor: pointer;
     }
 `
