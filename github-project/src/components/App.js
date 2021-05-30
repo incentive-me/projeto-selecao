886098ee9849bd5b1ef37951cac49d9d37bde205
Hugo Otávio <hugo.otavio08@gmail.com>
2021-05-29 23:12:51 -0300

    Criando Header

--- github-project/src/components/App.js
+++ github-project/src/components/App.js
@@ -1,9 +1,13 @@
 import React from 'react'
+import Header from './Header'
+import Main from './Main'
+
 
 function App() {
   return (
-    <div className="App">
-     
+    <div className="app bg-color-gray">
+      <Header />
+      <Main />
     </div>
   );
 }
---
+++ github-project/src/components/Header/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Header = () =>
+    <header className="bg-color-primary">        
+            <div className="container d-flex align-items-center justify-content-between text-white h-100">
+                <h3>GitHub Stars</h3>
+                <div className="">
+                    <button type="button" className="btn btn-light bg-color">Login</button>
+                </div>
+            </div>        
+    </header>
+
+export default Header
