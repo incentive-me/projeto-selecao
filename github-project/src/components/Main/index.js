76ef1938e4b2f8845d420032cdc3806c1e24b997
Hugo Otávio <hugo.otavio08@gmail.com>
2021-05-30 00:39:45 -0300

    Iniciando desenvolvimento Main

--- github-project/src/components/Main/index.js
+++ github-project/src/components/Main/index.js
@@ -1,5 +1,15 @@
 import React from 'react'
+import Icons from '../Icons'
 
- const Main = () => <h1>Main</h1>
+const Main = () =>
+    <main className="container py-5">
+        <div className="d-flex align-items-center justify-content-center h-100 bg-white">
+            <div className="d-flex flex-column align-items-center">
+                <h1 className="text-color-primary m-0 p-0">Organize seu GitHub</h1>  
+                <Icons size={100} icon="github" color="var(--color-primary)"/>        
+                <button className="btn btn-lg btn-secondary bg-color-primary mb-5 mt-1">Acessar Conta</button>
+            </div>
+        </div>
+    </main>
 
- export default Main
+export default Main
