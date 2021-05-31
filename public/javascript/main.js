050a55e4bacb22f7c2ee470a05bd9e84b672f116
Nathan Firmo <nathanssfirmo@gmail.com>
2021-05-30 22:11:57 -0300

    :tada: adicionando responsividade

--- public/javascript/main.js
+++ public/javascript/main.js
@@ -1,29 +1,55 @@
-function quemSomos() {
+function index() {
 document.getElementById("index").style.display = "flex"
 document.getElementById("copyCode").style.display = "none"
 document.getElementById("copyCss").style.display = "none"
 document.getElementById("howItWorks").style.display = "none"
+document.getElementById("test").style.display = "none"
+document.getElementById("ourPolicy").style.display = "none"
 }
 
-function importarHTML(){
+function copyCode(){
     document.getElementById("index").style.display = "none"
     document.getElementById("copyCode").style.display = "block"
     document.getElementById("copyCss").style.display = "none"
     document.getElementById("howItWorks").style.display = "none"
+    document.getElementById("test").style.display = "none"
+    document.getElementById("ourPolicy").style.display = "none"
 }
 
-function importarCSS(){
+function copyCss(){
     document.getElementById("index").style.display = "none"
     document.getElementById("copyCode").style.display = "none"
     document.getElementById("copyCss").style.display = "block"
     document.getElementById("howItWorks").style.display = "none"
+    document.getElementById("test").style.display = "none"
+    document.getElementById("ourPolicy").style.display = "none"
 }
 
-function sobreANossaAPI(){
+function howItWorks(){
     document.getElementById("index").style.display = "none"
     document.getElementById("copyCode").style.display = "none"
     document.getElementById("copyCss").style.display = "none"
     document.getElementById("howItWorks").style.display = "block"
+    document.getElementById("test").style.display = "none"
+    document.getElementById("ourPolicy").style.display = "none"
+}
+
+function test(){
+    document.getElementById("index").style.display = "none"
+    document.getElementById("copyCode").style.display = "none"
+    document.getElementById("copyCss").style.display = "none"
+    document.getElementById("howItWorks").style.display = "none"
+    document.getElementById("test").style.display = "block"
+    document.getElementById("ourPolicy").style.display = "none"
+}
+
+function ourPolicy(){
+    document.getElementById("index").style.display = "none"
+    document.getElementById("copyCode").style.display = "none"
+    document.getElementById("copyCss").style.display = "none"
+    document.getElementById("howItWorks").style.display = "none"
+    document.getElementById("test").style.display = "none"
+    document.getElementById("ourPolicy").style.display = "block"
 }
 
 function copyHTML() {
