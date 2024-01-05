2e8c6fe4609afa65cba86bb872f7bbd12b9c6b95
Oséias Costa <oseiasc2@gmail.com>
2024-01-05 12:34:20 -0300

    feat(server.ts): included new route login and dotenv

--- api/server.ts
+++ api/server.ts
@@ -1,29 +1,30 @@
 import express, { Request, Response } from 'express'
+import dotenv from 'dotenv'
+dotenv.config()
+import cors from 'cors'
+
+console.log(dotenv.config())
+
 import { UserUseCase } from './application/user.usecase'
 import UserRepository from './infra/repository/user.repository'
 import { UserController } from './presentation/user.controller'
 
 const app = express()
-
 app.use(express.json())
+app.use(cors())
 
-const userUseCase = new UserUseCase()
-const userRepo = new UserRepository(userUseCase)
-const userController = new UserController(userRepo)
+const userRepo = new UserRepository()
+const userUseCase = new UserUseCase(userRepo)
+const userController = new UserController(userUseCase)
 
 app.post("/user", (req, res) => userController.CreateUser(req, res))
+app.post("/login", (req, res) => userController.LoginUser(req, res))
 
-// const user = userRepo.CreateUserRepo({
-//     id: "123",
-//     name: "Oséias Costa",
-//     email: "osieas2@gmail.com",
-//     password: "1234"
-// })
 
 app.get("/user", (req: Request, res: Response) => {
     return res.status(200).send("Hello Wordo")
 })
 
 app.listen(3001, () => {
-    console.log("Server is running on port 3000")
+    console.log("Server is running on port 3001")
 })
