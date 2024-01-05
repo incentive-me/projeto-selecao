d168110b2b505ef236e8226646e07002b3d3d3c6
Oséias Costa <oseiasc2@gmail.com>
2024-01-05 10:56:48 -0300

    fix(user.controller.ts): call usecase correctly

--- api/presentation/user.controller.ts
+++ api/presentation/user.controller.ts
@@ -1,26 +1,39 @@
+import { UserUseCase } from "../application/user.usecase";
 import { User } from "../domain/user.entity";
-import UserRepository from "../infra/repository/user.repository";
 import { Request, Response } from "express";
 
 interface UserControllerInterface {
   CreateUser(req: Request, res: Response): Promise<void>;
+  LoginUser(req: Request, res: Response): Promise<void>;
 }
 
 export class UserController implements UserControllerInterface {
 
-    constructor(private userRepo: UserRepository) {
-      this.userRepo = userRepo
+    constructor(private userUseCase: UserUseCase) {
+      this.userUseCase = userUseCase
     }
     
     async CreateUser(req: Request, res: Response) {
-      const user: User = req.body;
+        const user: User = req.body;
 
-      try {
-        const newUser = await this.userRepo.CreateUserRepo(user)
-        res.status(200).send(newUser)
+        try {
+          const newUser = await this.userUseCase.CreateUser(user)
+          res.status(200).send(newUser)
 
-      } catch(err) {
-        res.status(400).json({error: err.message})
-      }
-  }
+        } catch(err) {
+          res.status(400).json({error: err.message})
+        }
+    }
+
+    async LoginUser(req: Request, res: Response): Promise<void> {
+        const { email, password } = req.body
+  
+        try {
+          // const login = await this.userUseCase.LoginUserRepo(email, password)
+          // res.status(200).send(login)
+
+        } catch(err) {
+          res.status(400).json({error: err.message})
+        }
+    }
 }
