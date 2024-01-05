9a51678064c98e05b077a1dfce07b2bdf762620a
Oséias Costa <oseiasc2@gmail.com>
2024-01-05 10:58:04 -0300

    fix(user.repository.ts): remove dependecies

--- api/infra/repository/user.repository.ts
+++ api/infra/repository/user.repository.ts
@@ -1,18 +1,29 @@
 import { UserUseCase } from "../../application/user.usecase";
 import { User } from "../../domain/user.entity";
+import { connection } from "../db/mysql";
 
 interface UserInterfaceRepo {
   CreateUserRepo(user: User): Promise<User | Error>;
+  LoginUserRepo(email: string, password: string): Promise<any>;
 }
 
 export default class UserRepository implements UserInterfaceRepo { 
-  constructor(private userUsecase: UserUseCase) {
-    this.userUsecase = userUsecase
-  }
 
   async CreateUserRepo(user: User): Promise<User | Error> {
-    const verify = await this.userUsecase.CreateUser(user)
-    return verify
+    const { id, name, email, password } = user
+
+    const [rows] = await connection.promise().query(
+      `INSERT INTO user (id, name, email, password) 
+      VALUES (?, ?, ?, ?)`, [id, name, email, password]
+    )
+    return user
   }
 
+  async LoginUserRepo(email: string, password: string): Promise<any> {
+      
+    const [rows] = await connection.promise().query(
+      `SELECT * FROM user WHERE email = ?`, [email]
+    )
+    return rows
+  }
 }
