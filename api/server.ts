88ad5f40e866f5e7a0e1dee0faeacaa4c1634d6d
unknown <oseiasc2@gmail.com>
2024-01-06 16:18:56 -0300

    feat(server.ts): included CreatePayment route

--- api/server.ts
+++ api/server.ts
@@ -10,6 +10,9 @@ import { middlewareJWT } from './presentation/middleware'
 import { BalanceController } from './presentation/balance.controller'
 import { BalanceUseCase } from './application/balance.usecase'
 import BalanceRepository from './infra/repository/balance.repository'
+import { PaymentUseCase } from './application/payment.usecase'
+import { PaymentController } from './presentation/payment.controller'
+import { PaymentRepository } from './infra/repository/payment.repository'
 
 const app = express()
 app.use(express.json())
@@ -23,13 +26,19 @@ const balanceRepo = new BalanceRepository()
 const balanceUseCase = new BalanceUseCase(balanceRepo)
 const balanceController = new BalanceController(balanceUseCase)
 
+const paymentRepository = new PaymentRepository()
+const paymentUseCase = new PaymentUseCase(paymentRepository)
+const paymentController = new PaymentController(paymentUseCase)
+
+app.post("/user", middlewareJWT, (req, res) => userController.CreateUser(req, res))
+app.post("/login", (req, res) => userController.LoginUser(req, res))
+
 app.delete("/balance/:id", middlewareJWT, (req, res) => balanceController.DeleteBalanceController(req, res))
 app.patch("/balance", middlewareJWT, (req, res) => balanceController.UpdateBalanceNameController(req, res))
 app.get("/balance", middlewareJWT, (req, res) => balanceController.GetAllBalancesController(req, res))
 app.post("/balance", middlewareJWT, (req, res) => balanceController.CreateBalanceController(req, res))
-app.post("/user", middlewareJWT, (req, res) => userController.CreateUser(req, res))
-app.post("/login", (req, res) => userController.LoginUser(req, res))
 
+app.post("/payment", middlewareJWT, (req, res) => paymentController.CreatePayment(req, res))
 
 app.get("/user", (req: Request, res: Response) => {
     return res.status(200).send("Hello Wordo")
