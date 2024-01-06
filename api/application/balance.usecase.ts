b41452525c2d95f17f6b0e7ad62b09a5382ebf7f
Oséias Costa <oseiasc2@gmail.com>
2024-01-05 22:28:16 -0300

    feat(balance.usecase): implement method GetallBalances and CreateBalance

--- api/application/balance.usecase.ts
+++ api/application/balance.usecase.ts
@@ -1,16 +1,19 @@
 import { Balance, BalanceInterface } from "../domain/balance.entity";
 import { v4 as uuid } from 'uuid'
 import { UserInfo } from "../domain/user.entity";
+import BalanceRepository from "../infra/repository/balance.repository";
 
 export class BalanceUseCase implements BalanceInterface {
-    constructor(){}
+    constructor(private balanceRepo: BalanceRepository){
+        this.balanceRepo = balanceRepo
+    }
 
-    CreateBalance(userInfo: UserInfo, balanceName: string, amount: number): Promise<Balance | Error> {
-        const newId = uuid()
+    async CreateBalance(userInfo: UserInfo, balanceName: string, amount: number): Promise<Balance | Error> {
+        const newId = uuid();
 
-        const verifyParams = BalanceUseCase.ValidateBalance(userInfo, balanceName, amount)
+        const verifyParams = BalanceUseCase.validateBalance(userInfo, balanceName, amount)
         if (!verifyParams) {
-            throw Error("")
+            throw Error("Balance invalid")
         }
         
         const newBalance: Balance = {
@@ -22,21 +25,25 @@ export class BalanceUseCase implements BalanceInterface {
             valueUsed: 0
         } 
 
+        const saveDb = await this.balanceRepo.CreateBalanceRepo(newBalance)
+
+        return newBalance
     }
 
-    GetAllBalnces(): Promise<Balance | Error> {
-        throw Error("test")
+    async GetAllBalances(userInfo: UserInfo): Promise<Balance | Error> {
+        const getBalances = await this.balanceRepo.GetAllBalances(userInfo)
+        return getBalances
     }
 
     DeleteBalance(id: string): Promise<boolean | Error> {
         throw Error("test")
     }
 
-    UpdateBalanceName(balance: Balance): Promise<Balance | Error> {
+    UpdateBalanceName(userInfo: UserInfo, balance: Balance, newName: string): Promise<Balance | Error> {
         throw Error("test")
     }
 
-    static ValidateBalance(userInfo: UserInfo, balanceName: string, amount: number): boolean | Error {
+    static validateBalance(userInfo: UserInfo, balanceName: string, amount: number): boolean | Error {
         if (!userInfo.id){
             throw Error("Id not found")
         }
@@ -46,8 +53,9 @@ export class BalanceUseCase implements BalanceInterface {
         }
 
         if (!amount){
-            throw Error("Balance Name is required")
+            throw Error("Balance Amount is required")
         }
+        return true
 
     } 
 
