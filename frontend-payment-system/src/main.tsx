781c9c1e4a42059626dc10decedf9e52a5f3bffa
Oséias Costa <oseiasc2@gmail.com>
2024-01-07 15:08:22 -0300

    feat(main.tsx): included RouterProvider with router

--- frontend-payment-system/src/main.tsx
+++ frontend-payment-system/src/main.tsx
@@ -4,12 +4,15 @@ import { ThemeProvider } from '@emotion/react';
 import { CssBaseline } from '@mui/material';
 import theme from './theme';
 import App from './App';
+import { RouterProvider } from 'react-router-dom';
+import { router } from './routes/RenderRouter';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <CssBaseline />
-      <App />
+      <RouterProvider router={router} />
+        <App />
     </ThemeProvider>
   </React.StrictMode>,
 );
