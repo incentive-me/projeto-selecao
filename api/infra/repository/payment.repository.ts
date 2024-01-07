ab2fa8450d8cf8f64fe518a9fc933850da0844a6
Oséias Costa <oseiasc2@gmail.com>
2024-01-07 07:39:47 -0300

    feat(payment.repository): create requests to db with methods DeletePayment and UpdatePaymentName

--- api/infra/repository/payment.repository.ts
+++ api/infra/repository/payment.repository.ts
@@ -1,9 +1,14 @@
 import { Payment } from "../../domain/payment.entity";
+import { UserInfo } from "../../domain/user.entity";
 import { connection } from "../db/mysql";
 
 
 interface PaymentRepositoryInterface {
     CreatePayment(payment: Payment): Promise<any>
+    GetallPayments(userInfo: UserInfo): Promise<any>
+    DeletePayment(id: string): Promise<any>
+    UpdatePaymentName(payment: Payment, newName: string): Promise<any>
+    VerifyBalanceAmount(payment: Payment): Promise<any>
 }
 
 export class PaymentRepository implements PaymentRepositoryInterface {
@@ -21,5 +26,31 @@ export class PaymentRepository implements PaymentRepositoryInterface {
             ]
         )
         return rows
+    }
+
+    async GetallPayments(userInfo: UserInfo): Promise<any>{
+        const [rows] = await connection.promise().query(
+            `SELECT * FROM payment WHERE userId = ?`, [userInfo.id]
+        )
+        return rows
+    }
+
+    async DeletePayment(id: string): Promise<any>{
+        const [rows] = await connection.promise().query(
+            `DELETE FROM payment WHERE id = ?`, [id]
+        )
+        return rows
+    }
+
+    async UpdatePaymentName(payment: Payment, newName: string): Promise<any>{
+        const [rows] = await connection.promise().query(
+            `UPDATE payment SET name = ? WHERE id = ?`, [newName, payment.id]
+        )
+      
+        if(rows) {
+            return true
+        }
+
+        return false
     }
 }
