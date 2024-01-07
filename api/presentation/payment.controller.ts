73cb20ae87f5553687e05faf3b8ffc97840b30d9
Oséias Costa <oseiasc2@gmail.com>
2024-01-07 07:34:05 -0300

    feat(payment.controller): implemented controllers DeletePayment and UpdatePayment

--- api/presentation/payment.controller.ts
+++ api/presentation/payment.controller.ts
@@ -2,7 +2,10 @@ import { Request, Response } from "express";
 import { PaymentUseCase } from "../application/payment.usecase";
 
 interface PaymentControllerInterface {
-    CreatePayment(req: Request, res: Response): Promise<void>    
+    CreatePayment(req: Request, res: Response): Promise<void>
+    GetAllPayments(req: Request, res: Response): Promise<void>
+    DeletePayment(req: Request, res: Response): Promise<void> 
+    UpdatePaymentName(req: Request, res: Response): Promise<void>   
 }
 
 export class PaymentController implements PaymentControllerInterface {
@@ -16,6 +19,39 @@ export class PaymentController implements PaymentControllerInterface {
         try {
             const createPay = await this.paymentUseCase.CreatePayment(userInfo, payment)
             res.status(201).send(createPay)
+        } catch(err) {
+            res.status(400).json({err: err.message})
+        }
+    }
+
+    async GetAllPayments(req: Request, res: Response): Promise<void>{
+        const { userInfo } = req.body
+
+        try {
+            const getAll = await this.paymentUseCase.GetAllPayments(userInfo)
+            res.status(200).send(getAll)
+        } catch(err) {
+            res.status(400).json({err: err.message})
+        }
+    }
+
+    async DeletePayment(req: Request, res: Response): Promise<void>{
+        const id = req.params.id
+
+        try {
+            const deletePayment = await this.paymentUseCase.DeletePayment(id)
+            res.status(200).json({deleted: true})
+        } catch(err) {
+            res.status(400).json({err: err.message})
+        }
+    }
+
+    async UpdatePaymentName(req: Request, res: Response): Promise<void>{
+        const { payment, newName } = req.body
+
+        try {
+            const updatePayment = await this.paymentUseCase.UpdatePaymentName(payment, newName)
+            res.status(200).send(updatePayment)
         } catch(err) {
             res.status(400).json({err: err.message})
         }
