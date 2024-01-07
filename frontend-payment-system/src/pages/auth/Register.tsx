3f2e9897ad9d0f7dab2b831554567349d2c41c85
Oséias Costa <oseiasc2@gmail.com>
2024-01-07 14:35:58 -0300

    feat(Register.tsx): created Register page

---
+++ frontend-payment-system/src/pages/auth/Register.tsx
@@ -0,0 +1,62 @@
+import { Visibility, VisibilityOff } from "@mui/icons-material";
+import { Box, Button, FormControl, IconButton, InputAdornment, InputLabel, OutlinedInput, TextField, Typography } from "@mui/material";
+import React from "react";
+
+export default function Register(){
+    const [showPassword, setShowPassword] = React.useState(false);
+
+    const handleClickShowPassword = () => setShowPassword((show) => !show);
+
+    const handleMouseDownPassword = (event: React.MouseEvent<HTMLButtonElement>) => {
+        event.preventDefault();
+    };
+
+    return(
+        <Box 
+            component="section" 
+            display='flex'
+            flexDirection="column"
+            alignItems="center"
+            paddingTop="100px"
+        >
+        <Typography paddingBottom="20px" variant="h4" component="h2">Payment System</Typography>
+        <Typography paddingBottom="20px" color="#556cd6" variant="h4" component="h4">Registrar</Typography>
+        <TextField
+            label="Nome"
+            id="outlined-start-adornment"
+            margin="normal" 
+            sx={{ m: 1, width: '35ch' }}
+        />
+        <TextField
+            label="Email"
+            id="outlined-start-adornment"
+            margin="normal" 
+            sx={{ m: 1, width: '35ch' }}
+        />
+        <TextField
+                label="Senha"
+                sx={{ m: 1, width: '35ch' }}
+                id="outlined-start-password"
+                type={showPassword ? 'text' : 'password'}
+                InputProps={{
+                    endAdornment: 
+                    <InputAdornment position="end">
+                        <IconButton
+                            aria-label="toggle password visibility"
+                            onClick={handleClickShowPassword}
+                            onMouseDown={handleMouseDownPassword}
+                        >
+                            {showPassword ? <VisibilityOff /> : <Visibility />}
+                        </IconButton>
+                    </InputAdornment>,
+            }}
+        />
+        <Button 
+            sx={{ m: 1, width: '40ch' }}
+            variant="contained" 
+            disabled={true}
+        >Registrar</Button>
+        <Button sx={{ m: 1, width: '40ch' }}>Voltar</Button>
+    </Box>
+    )
+}
