b44c3938dac0eb02c8ed716ce5d8f0496e383e05
Oséias Costa <oseiasc2@gmail.com>
2024-01-07 15:06:07 -0300

    feat(PrivateRoute.tsx): create private routes

---
+++ frontend-payment-system/src/routes/PrivateRoute.tsx
@@ -0,0 +1,7 @@
+import { Navigate, Outlet } from "react-router-dom";
+
+export const PrivateRoute = () => {
+  const user = true  
+
+  return user ? <Outlet /> : <Navigate to="/entrar" replace />;
+};
