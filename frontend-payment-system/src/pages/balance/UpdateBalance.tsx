54f301b9d9eb8a5ff61301774268cbbcc5e7b80b
Oséias Costa <oseiasc2@gmail.com>
2024-01-08 10:20:52 -0300

    feat(UpdateBalance): create page UpdateBalance

---
+++ frontend-payment-system/src/pages/balance/UpdateBalance.tsx
@@ -0,0 +1,20 @@
+import { Box, TextField } from "@mui/material";
+import Title from "../../components/Title";
+import FormContainer from "../../components/FormContainer";
+import SelectBalance from "../../components/SelectBalance";
+import FormButtons from "../../components/FormButtons";
+
+export default function UpdatePayment(){
+    return(
+        <Box component="section">
+            <Title title="Editar saldo" />
+            <FormContainer>
+                <Box component="div">
+                    <TextField label="Nome" sx={{width: '100%', paddingBottom: "16px" }}/>
+                    <TextField disabled={true} label="Valor" sx={{width: '100%', paddingBottom: "16px" }}/>
+                </Box>
+                <FormButtons />
+            </FormContainer>
+        </Box>
+    )
+}
