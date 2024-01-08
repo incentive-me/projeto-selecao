8bc7e43154183ed79a7cdbfe3759cb9e46f5eed9
Oséias Costa <oseiasc2@gmail.com>
2024-01-08 07:05:53 -0300

    fix(Title.tsx): remove padding

--- frontend-payment-system/src/components/Title.tsx
+++ frontend-payment-system/src/components/Title.tsx
@@ -2,14 +2,7 @@ import { Typography } from "@mui/material";
 
 export default function Title({title}: {title: string}){
     return(
-        <Typography style={{
-                paddingTop: "24px",
-                paddingLeft: "24px",
-                paddingRight: "24px",
-                color: "#000000de",
-                fontSize: "24px"
-            }}
-        >
+        <Typography style={{ color: "#000000de", fontSize: "24px" }}>
             {title}
         </Typography>
     )
