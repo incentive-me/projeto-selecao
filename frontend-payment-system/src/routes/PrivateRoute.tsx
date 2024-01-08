db712db21d75a5b237a9d5cd9233da2fda0693ee
unknown <oseiasc2@gmail.com>
2024-01-08 17:52:13 -0300

    feat(PrivateRoute.tsx): included global state user

--- frontend-payment-system/src/routes/PrivateRoute.tsx
+++ frontend-payment-system/src/routes/PrivateRoute.tsx
@@ -1,7 +1,8 @@
+import { useSelector } from "react-redux";
 import { Navigate, Outlet } from "react-router-dom";
 
 export const PrivateRoute = () => {
-  const user = false  
+  const user = useSelector((state: any) => state.user.user)
 
   return user ? <Outlet /> : <Navigate to="/entrar" />;
 };
