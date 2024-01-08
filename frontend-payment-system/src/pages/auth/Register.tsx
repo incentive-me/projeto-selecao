ac2959c50a60ecc44ac2ea94740578eb64619a64
unknown <oseiasc2@gmail.com>
2024-01-08 17:58:41 -0300

    feat(Register.tsx): included user global state

--- frontend-payment-system/src/pages/auth/Register.tsx
+++ frontend-payment-system/src/pages/auth/Register.tsx
@@ -2,32 +2,36 @@ import { Visibility, VisibilityOff } from "@mui/icons-material";
 import { Link, Navigate } from "react-router-dom";
 import { Box, Button, IconButton, InputAdornment, TextField, Typography } from "@mui/material";
 import React, { useState } from "react";
-import axios, { AxiosError, AxiosResponse } from "axios";
+import axios from "axios";
+import { findField } from "../../utils/register";
+import { useDispatch, useSelector } from "react-redux";
 
 export default function Register(){
-    const user = false
     const [showPassword, setShowPassword] = React.useState(false);
+    const [error, setError] = useState("")
     const [resgisterData, setRegisterData] = useState({
         name: "", 
         email: "", 
         password: ""
     })
-
+    const dispatch = useDispatch()
+    const user = useSelector((state: any) => state.user.user)
+    
     const handleClickShowPassword = () => setShowPassword((show) => !show);
-
+    
     const handleMouseDownPassword = (event: React.MouseEvent<HTMLButtonElement>) => {
         event.preventDefault();
     };
-
-    const register = async () => {
-            const res: any = await axios.post("http://localhost:3001/user",resgisterData, 
+    
+    const register = () => {
+        axios.post("http://localhost:3002/user", resgisterData, 
             { headers: { 'Content-Type': 'application/json' }})
-            
-            console.log(res)
+                .then((res) => console.log(res.data))
+                .catch((err) => setError(err.response.data.error))
+        }
+    const errorField = findField(error)
         
-    }
-
-    if (user) {
+        if (user) {
         return <Navigate to="/pagamentos" replace={true} />
       }
 
@@ -43,20 +47,26 @@ export default function Register(){
         <Typography paddingBottom="20px" color="#556cd6" variant="h4" component="h4">Registrar</Typography>
         <TextField
             label="Nome"
-            onChange={(e: React.ChangeEvent<HTMLInputElement>) => 
-                setRegisterData({...resgisterData, name: e.target.value})}
+            error={errorField === "Name"}
+            onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+                setError("")
+                setRegisterData({...resgisterData, name: e.target.value})}}
             sx={{ m: 1, width: '35ch' }}
         />
         <TextField
             label="Email"
-            onChange={(e: React.ChangeEvent<HTMLInputElement>) => 
-                setRegisterData({...resgisterData, email: e.target.value})}
+            error={errorField === "Email" || errorField === "Duplicate"}
+            onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+                setError("")
+                setRegisterData({...resgisterData, email: e.target.value})}}
             sx={{ m: 1, width: '35ch' }}
         />
         <TextField
                 label="Senha"
-                onChange={(e: React.ChangeEvent<HTMLInputElement>) => 
-                    setRegisterData({...resgisterData, password: e.target.value})}
+                error={errorField === "Password"}
+                onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+                    setError("")
+                    setRegisterData({...resgisterData, password: e.target.value})}}
                 sx={{ m: 1, width: '35ch' }}
                 type={showPassword ? 'text' : 'password'}
                 InputProps={{
