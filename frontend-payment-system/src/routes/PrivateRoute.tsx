89f54e074316b1bf18e721f4fc0567faf5a96fcc
Oséias Costa <oseiasc2@gmail.com>
2024-01-07 21:58:12 -0300

    fix(PrivateRoute): set user to true for tests

--- frontend-payment-system/src/routes/PrivateRoute.tsx
+++ frontend-payment-system/src/routes/PrivateRoute.tsx
@@ -3,5 +3,5 @@ import { Navigate, Outlet } from "react-router-dom";
 export const PrivateRoute = () => {
   const user = true  
 
-  return user ? <Outlet /> : <Navigate to="/entrar" replace />;
+  return user ? <Outlet /> : <Navigate to="/entrar" />;
 };
