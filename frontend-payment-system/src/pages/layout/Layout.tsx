d191f4789ffea3a1cd48aa105461e67cfd22e2a7
Oséias Costa <oseiasc2@gmail.com>
2024-01-08 10:53:25 -0300

    fix(Layout): change button hover color

--- frontend-payment-system/src/pages/layout/Layout.tsx
+++ frontend-payment-system/src/pages/layout/Layout.tsx
@@ -1,10 +1,12 @@
 import { Box, Button, IconButton, Typography } from "@mui/material";
-import { Outlet } from "react-router-dom";
+import { Link, Outlet, useLocation } from "react-router-dom";
 import PersonIcon from '@mui/icons-material/Person';
 import { layoutStyle as S } from "../../styles/layout.style";
 import { AccountBalanceWalletSharp, LogoutSharp, MonetizationOnSharp } from "@mui/icons-material";
 
 export default function Layout() {
+    const location = useLocation()
+    const path = location.pathname.split("/")[1]
     return(
         <Box component="section">
             <Box sx={S.header.container}>
@@ -17,14 +19,32 @@ export default function Layout() {
             </Box>
             <Box sx={S.body} component="div">
                 <Box sx={S.menu.container} component="nav">
-                        <Button sx={[S.menu.buttonBox, {marginTop: "8px"}]} component="li">
+                    <Link to="/pagamentos" style={{textDecoration: "none"}}>
+                        <Button 
+                            sx={[
+                                S.menu.buttonBox, {
+                                    marginTop: "8px", 
+                                    backgroundColor: path === "pagamentos" ? "#90caf929" : "none"
+                            }]} 
+                            component="li"
+                        >
                             <MonetizationOnSharp style={S.menu.icon} />
                             <Typography style={{textTransform: "capitalize", color: "#fff"}}>Pagamentos</Typography>
                         </Button>
-                        <Button sx={[S.menu.buttonBox, {borderBottom: "1px solid #0000001f"}]} component="li">
+                    </Link>
+                    <Link to="saldos" style={{textDecoration: "none"}}>
+                        <Button 
+                            sx={[
+                                S.menu.buttonBox, {
+                                    borderBottom: "1px solid #0000001f",
+                                    backgroundColor: path === "saldos" ? "#90caf929" : "none"
+                            }]} 
+                            component="li"
+                        >
                             <AccountBalanceWalletSharp style={S.menu.icon} />
                             <Typography style={{textTransform: "capitalize", color: "#fff"}}>Saldos</Typography>
                         </Button>
+                    </Link>
                         <Button sx={[S.menu.buttonBox, {marginTop: "8px"}]} component="li">
                             <LogoutSharp style={S.menu.icon} />
                             <Typography style={{textTransform: "capitalize", color: "#fff"}}>Sair da conta</Typography>
