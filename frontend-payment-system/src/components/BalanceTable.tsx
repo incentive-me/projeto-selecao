14ede4b57d6b9b0d8971a3b94e2963e53ddd4cb5
Oséias Costa <oseiasc2@gmail.com>
2024-01-08 09:46:28 -0300

    feat(BalanceTable.tsx): create table to Balance

---
+++ frontend-payment-system/src/components/BalanceTable.tsx
@@ -0,0 +1,144 @@
+import * as React from 'react';
+import Paper from '@mui/material/Paper';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TablePagination from '@mui/material/TablePagination';
+import TableRow from '@mui/material/TableRow';
+import { IconButton } from '@mui/material';
+import { Delete, Edit } from '@mui/icons-material';
+
+interface Column {
+  id: 'name' | 'description' | 'initialValue' | 'valueUsed' | 'totalValue';
+  label: string;
+  minWidth?: number;
+  align?: 'right';
+  format?: (value: number) => string;
+}
+
+const columns: readonly Column[] = [
+  { id: 'name', label: 'Nome', minWidth: 40 },
+  { id: 'description', label: 'Descrição', minWidth: 40 },
+  {
+    id: 'initialValue',
+    label: 'Valor',
+    minWidth: 40,
+    align: 'right',
+    format: (value: number) => value.toLocaleString('en-US'),
+  },
+  {
+    id: 'valueUsed',
+    label: 'Ações',
+    minWidth: 40,
+    align: 'right',
+  },
+  {
+    id: 'totalValue',
+    label: 'Ações',
+    minWidth: 40,
+    align: 'right',
+  }
+];
+
+interface Data {
+  name: string;
+  description: string;
+  initialValue: number;
+  valueUsed: number;
+  totalValue: number;
+}
+
+function createData(
+  name: string,
+  description: string,
+  initialValue: number,
+  valueUsed: number,
+  totalValue: number
+): Data {
+  return { name, description, initialValue, valueUsed, totalValue };
+}
+
+const rows = [
+  createData('Pedido de Pagamento A', 'Insira uma descrição aqui', 2500, 4000, 2000),
+  createData('Pedido de Pagamento B', 'Insira uma descrição aqui', 1500, 4000, 2000),
+  createData('Pedido de Pagamento C', 'Insira uma descrição aqui', 3500, 4000, 2000),
+];
+
+export default function BalanceTable() {
+  const [page, setPage] = React.useState(0);
+  const [rowsPerPage, setRowsPerPage] = React.useState(10);
+
+  const handleChangePage = (event: unknown, newPage: number) => {
+    setPage(newPage);
+  };
+
+  const handleChangeRowsPerPage = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setRowsPerPage(+event.target.value);
+    setPage(0);
+  };
+
+  return (
+    <Paper sx={style.container}>
+      <TableContainer sx={{ maxHeight: 440 }}>
+        <Table stickyHeader aria-label="sticky table">
+          <TableHead>
+            <TableRow>
+              {columns.map((column) => (
+                <TableCell
+                  key={column.id}
+                  align={column.align}
+                  style={{ minWidth: column.minWidth }}
+                >
+                  {column.label}
+                </TableCell>
+              ))}
+              <TableCell>Ações</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {rows
+              .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+              .map((row) => {
+                return (
+                  <TableRow hover role="checkbox" tabIndex={-1} key={row.name}>
+                    {columns.map((column) => {
+                      const value = row[column.id];
+                      return (
+                        <TableCell key={column.id} align={column.align}>
+                          {column.format && typeof value === 'number'
+                            ? column.format(value)
+                            : value}
+                        </TableCell>
+                      );
+                    })}
+                    <TableCell>
+                        <IconButton><Edit /></IconButton>
+                        <IconButton><Delete /></IconButton>
+                    </TableCell>
+                </TableRow>
+                );
+              })}
+          </TableBody>
+        </Table>
+      </TableContainer>
+      <TablePagination
+        rowsPerPageOptions={[10, 25, 100]}
+        component="div"
+        count={rows.length}
+        rowsPerPage={rowsPerPage}
+        page={page}
+        onPageChange={handleChangePage}
+        onRowsPerPageChange={handleChangeRowsPerPage}
+      />
+    </Paper>
+  );
+}
+
+const style = {
+  container: {
+    width: '100%', 
+    overflow: 'hidden'
+  }
+}
