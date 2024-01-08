ce523f90929e694d98bc1187ded456f05405fbd6
Adrian Braga <adrian.braga.ro@gmail.com>
2024-01-08 14:27:06 -0400

    feat: Add mock datas and implementation calculete

--- bank-incentiveme/web/src/components/Dashboard.tsx
+++ bank-incentiveme/web/src/components/Dashboard.tsx
@@ -1,33 +1,59 @@
 import { ArrowCircleDown, ArrowCircleUp, CurrencyDollar } from 'phosphor-react';
+import { useMemo } from 'react';
+
+import { transactions } from '../../mock-dashboard.json';
+import { priceFormatter } from '../utils/fomatter';
 
 export function Dashboard() {
+  const summary = useMemo(() => {
+    return transactions.reduce(
+      (acc, transaction) => {
+        if (transaction.type === 'income') {
+          acc.income += transaction.price
+          acc.total += transaction.price
+        } else {
+          acc.outcome += transaction.price
+          acc.total -= transaction.price
+        }
+
+        return acc
+      },
+      {
+        income: 0,
+        outcome: 0,
+        total: 0,
+      },
+    )
+  }, [transactions])
+
+
   return (
     <div className='flex justify-around items-center my-12'>
-      <div className='flex flex-col justify-start items-start inline-flex gap-3 py-6 px-8  bg-zinc-800 rounded-md'>
+      <div className='w-full flex-col justify-start items-start inline-flex gap-3 py-6 px-8  bg-zinc-800 rounded-md'>
         <div className='self-stretch justify-between items-start inline-flex'>
           <span className='text-stone-300 text-base'>Entradas</span>
           <ArrowCircleUp size={32} color='#00B37E' />
         </div>
 
-        <strong className='text-zinc-200 text-[32px] font-bold'>R$ 10.000,00</strong>
+        <strong className='text-zinc-200 text-[32px] font-bold'>{priceFormatter.format(summary.income)}</strong>
       </div>
 
-      <div className='flex flex-col justify-start items-start inline-flex gap-3 py-6 px-8  bg-zinc-800 rounded-md'>
+      <div className='w-full mx-6 flex-col justify-start items-start inline-flex gap-3 py-6 px-8  bg-zinc-800 rounded-md'>
         <div className='self-stretch justify-between items-start inline-flex'>
-          <span className='text-stone-300 text-base'>Saidas</span>
+          <span className='text-stone-300 text-base'>Saídas</span>
           <ArrowCircleDown size={32} color='#F75A68' />
         </div>
 
-        <strong className=' text-zinc-200 text-[32px] font-bold'>R$ 5.000,00</strong>
+        <strong className=' text-zinc-200 text-[32px] font-bold'>{priceFormatter.format(summary.outcome)}</strong>
       </div>
 
-      <div className='flex flex-col justify-start items-start inline-flex gap-3 py-6 px-8  bg-emerald-800 rounded-md'>
+      <div className='w-full flex-col justify-start items-start inline-flex gap-3 py-6 px-8  bg-emerald-800 rounded-md'>
         <div className='self-stretch justify-between items-start inline-flex'>
           <span className='text-stone-300 text-base'>Total</span>
           <CurrencyDollar size={32} color='#FFF' />
         </div>
 
-        <strong className='text-zinc-200 text-[32px] font-bold'>R$ 5.000,00</strong>
+        <strong className='text-zinc-200 text-[32px] font-bold'>{priceFormatter.format(summary.total)}</strong>
       </div>
     </div>
   )
