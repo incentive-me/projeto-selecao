a895d7149ba0052646cdb916846473f8e5b62b0c
Oséias Costa <oseiasc2@gmail.com>
2024-01-08 13:06:44 -0300

    feat(Register): implemented function register

--- frontend-payment-system/src/pages/auth/Register.tsx
+++ frontend-payment-system/src/pages/auth/Register.tsx
@@ -1,17 +1,31 @@
 import { Visibility, VisibilityOff } from "@mui/icons-material";
-import { Navigate, redirect, useNavigate } from "react-router-dom";
-import { Box, Button, FormControl, IconButton, InputAdornment, InputLabel, OutlinedInput, TextField, Typography } from "@mui/material";
-import React from "react";
+import { Link, Navigate } from "react-router-dom";
+import { Box, Button, IconButton, InputAdornment, TextField, Typography } from "@mui/material";
+import React, { useState } from "react";
+import axios, { AxiosError, AxiosResponse } from "axios";
 
 export default function Register(){
-    const user = true
+    const user = false
     const [showPassword, setShowPassword] = React.useState(false);
+    const [resgisterData, setRegisterData] = useState({
+        name: "", 
+        email: "", 
+        password: ""
+    })
 
     const handleClickShowPassword = () => setShowPassword((show) => !show);
 
     const handleMouseDownPassword = (event: React.MouseEvent<HTMLButtonElement>) => {
         event.preventDefault();
     };
+
+    const register = async () => {
+            const res: any = await axios.post("http://localhost:3001/user",resgisterData, 
+            { headers: { 'Content-Type': 'application/json' }})
+            
+            console.log(res)
+        
+    }
 
     if (user) {
         return <Navigate to="/pagamentos" replace={true} />
@@ -29,20 +43,21 @@ export default function Register(){
         <Typography paddingBottom="20px" color="#556cd6" variant="h4" component="h4">Registrar</Typography>
         <TextField
             label="Nome"
-            id="outlined-start-adornment"
-            margin="normal" 
+            onChange={(e: React.ChangeEvent<HTMLInputElement>) => 
+                setRegisterData({...resgisterData, name: e.target.value})}
             sx={{ m: 1, width: '35ch' }}
         />
         <TextField
             label="Email"
-            id="outlined-start-adornment"
-            margin="normal" 
+            onChange={(e: React.ChangeEvent<HTMLInputElement>) => 
+                setRegisterData({...resgisterData, email: e.target.value})}
             sx={{ m: 1, width: '35ch' }}
         />
         <TextField
                 label="Senha"
+                onChange={(e: React.ChangeEvent<HTMLInputElement>) => 
+                    setRegisterData({...resgisterData, password: e.target.value})}
                 sx={{ m: 1, width: '35ch' }}
-                id="outlined-start-password"
                 type={showPassword ? 'text' : 'password'}
                 InputProps={{
                     endAdornment: 
@@ -60,9 +75,12 @@ export default function Register(){
         <Button 
             sx={{ m: 1, width: '40ch' }}
             variant="contained" 
-            disabled={true}
+            onClick={register}
+            disabled={false}
         >Registrar</Button>
-        <Button sx={{ m: 1, width: '40ch' }}>Voltar</Button>
+        <Link to="/entrar">
+            <Button sx={{ m: 1, width: '40ch' }}>Voltar</Button>
+        </Link>
     </Box>
     )
 }
