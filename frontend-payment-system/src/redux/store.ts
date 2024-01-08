6b1fc9642a493b2b6ebfe4afd1bde5623039b084
unknown <oseiasc2@gmail.com>
2024-01-08 17:49:19 -0300

    feat(store.tsx): create redux store

---
+++ frontend-payment-system/src/redux/store.ts
@@ -0,0 +1,10 @@
+import { configureStore } from "@reduxjs/toolkit";
+import userSlice from "./user.slice";
+
+export const store = configureStore({
+    reducer: {
+        user: userSlice
+    }})
+
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
