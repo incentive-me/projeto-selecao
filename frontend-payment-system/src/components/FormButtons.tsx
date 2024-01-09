b1edda34d0645ab3f81c319d184f3e5f1a02d5e5
Oséias Costa <oseiasc2@gmail.com>
2024-01-09 11:37:38 -0300

    fix(FormButtons): included props path, textButton and onClick

--- frontend-payment-system/src/components/FormButtons.tsx
+++ frontend-payment-system/src/components/FormButtons.tsx
@@ -1,10 +1,19 @@
 import { Box, Button } from "@mui/material";
+import { Link } from "react-router-dom";
 
-export default function FormButtons(){
+export default function FormButtons({path, textButton, onClick}: FormButtonsProps){
     return(
         <Box sx={{display: "flex", justifyContent: "space-between"}}>
-            <Button variant="outlined">Cancelar</Button>
-            <Button variant="contained">Criar</Button>
+            <Link to={path}>
+                <Button variant="outlined">Cancelar</Button>
+            </Link>
+            <Button variant="contained" onClick={onClick}>{textButton}</Button>
         </Box>
     )
+}
+
+type FormButtonsProps = {
+    path: string, 
+    textButton: string,
+    onClick:  React.MouseEventHandler<HTMLButtonElement>
 }
