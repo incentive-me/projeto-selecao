b4a3220b0f9a14e7a518b65b0441e4566b74b112
Oséias Costa <oseiasc2@gmail.com>
2024-01-09 12:18:30 -0300

    feat(UpdateBalance): create balance state to change name, and onChange on TextField

--- frontend-payment-system/src/pages/balance/UpdateBalance.tsx
+++ frontend-payment-system/src/pages/balance/UpdateBalance.tsx
@@ -1,19 +1,54 @@
 import { Box, TextField } from "@mui/material";
 import Title from "../../components/Title";
 import FormContainer from "../../components/FormContainer";
-import SelectBalance from "../../components/SelectBalance";
 import FormButtons from "../../components/FormButtons";
+import { useLocation } from "react-router-dom";
+import { inputStyle } from "../../styles/global.style";
+import React, { useState } from "react";
+import { httpClient } from "../../utils/http";
 
 export default function UpdateBalance(){
+    const { state } = useLocation()
+    const [ newName, setNewName ] = useState(state.balanceName)
+    
+    const test = { balance: state, newName}
+    console.log(test)
+
+    const handleUpdateName = () => {
+        httpClient("balance", "POST", test).then(res => console.log(res)).catch(err => console.log("err", err))
+        // fetch("http://localhost:3001/balance", {
+        //     method: "POST",
+        //     headers: {
+        //         "Content-Type": "application/json",
+        //     },
+        //     body: JSON.stringify(test)
+        // }).then(res => console.log("res", res)).catch(err => console.log("err", err))
+    }
+
     return(
         <Box component="section">
             <Title title="Editar saldo" />
             <FormContainer>
                 <Box component="div">
-                    <TextField label="Nome" sx={{width: '100%', paddingBottom: "16px" }}/>
-                    <TextField disabled={true} label="Valor" sx={{width: '100%', paddingBottom: "16px" }}/>
+                    <TextField 
+                        label="Nome" 
+                        sx={inputStyle}
+                        value={newName}
+                        onChange={(e: React.ChangeEvent<HTMLInputElement>) => 
+                            setNewName(e.target.value)}
+                    />
+                    <TextField 
+                        disabled={true} 
+                        label="Valor"
+                        sx={inputStyle}
+                        defaultValue={state.totalValue}
+                    />
                 </Box>
-                <FormButtons />
+                <FormButtons 
+                    path="/saldos" 
+                    textButton="salvar" 
+                    onClick={handleUpdateName} 
+                />
             </FormContainer>
         </Box>
     )
