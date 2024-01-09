82d206e1647cd72f4840129f4439f59f4e246aa8
Oséias Costa <oseiasc2@gmail.com>
2024-01-09 14:11:20 -0300

    feat(App): included variable token

--- frontend-payment-system/src/App.tsx
+++ frontend-payment-system/src/App.tsx
@@ -1,16 +1,10 @@
-import * as React from 'react';
-import Container from '@mui/material/Container';
-import Typography from '@mui/material/Typography';
-import Box from '@mui/material/Box';
-import Link from '@mui/material/Link';
-import ProTip from './ProTip';
-import Login from './pages/auth/Login';
-import Register from './pages/auth/Register';
-import Layout from './pages/layout/Layout';
+import { useEffect } from "react";
+
 
 export default function App() {
+  const getToken = localStorage.getItem("paymentsToken")
+
   return (
-    <Container maxWidth="sm">
-    </Container>
+    <></>
   );
 }
