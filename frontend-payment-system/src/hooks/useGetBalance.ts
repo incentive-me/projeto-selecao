129d0f46adfa77f414d7cc41143b2399ca5e7931
unknown <oseiasc2@gmail.com>
2024-01-09 16:11:25 -0300

    fix(useGetBalance.tsx): fixed use global state

--- frontend-payment-system/src/hooks/useGetBalance.ts
+++ frontend-payment-system/src/hooks/useGetBalance.ts
@@ -1,21 +1,19 @@
 import { useDispatch, useSelector } from "react-redux"
 import { RootState } from "../redux/store"
-import { useEffect, useState } from "react"
+import { useEffect } from "react"
 import { httpClient } from "../utils/http"
-import { Balance, fetchBalances } from "../redux/balance.slice"
+import { fetchBalances } from "../redux/balance.slice"
 
 export function useGetBalance(){
     const dispatch = useDispatch()
-    const [ balance, setBalance ] = useState()
+    const balance = useSelector((state:RootState) => state?.balance)
 
-    useEffect(() => {
-        if(!balance){
+    useEffect(()=> {
+        if(balance.balance.length === 0){
             httpClient("balance", "get", {})
-                .then((res) => {
-                    setBalance(res.data)
-                    return dispatch(fetchBalances(res.data))})
+            .then((res) => dispatch(fetchBalances(res.data)))
         }
     },[])
 
-    return balance
+    return balance.balance
 }
