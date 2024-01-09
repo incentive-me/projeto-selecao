dbfdac985dfc708b48d9afe4ecb468a3372ca027
unknown <oseiasc2@gmail.com>
2024-01-09 18:05:47 -0300

    feat(UpdatePayment): created state to newName

--- frontend-payment-system/src/pages/payment/UpdatePayment.tsx
+++ frontend-payment-system/src/pages/payment/UpdatePayment.tsx
@@ -3,19 +3,54 @@ import Title from "../../components/Title";
 import FormContainer from "../../components/FormContainer";
 import SelectBalance from "../../components/SelectBalance";
 import FormButtons from "../../components/FormButtons";
+import { useDispatch } from "react-redux";
+import { useLocation } from "react-router-dom";
+import { useState } from "react";
+import { inputStyle } from "../../styles/global.style";
+import { httpClient } from "../../utils/http";
+import { updatePaymentName } from "../../redux/payment.slice";
 
 export default function UpdatePayment(){
+    const dispatch = useDispatch()
+    const { state } = useLocation()
+    const [ newName, setNewName ] = useState(state.name)
+   console.log(state)
+    const handleUpdatePaymentName = () => {
+        httpClient("payment", "PATCH", {state, newName})
+            .then((res) => dispatch(updatePaymentName(res.data)))
+    }
+
     return(
         <Box component="section">
             <Title title="Editar pedido de pagamento" />
             <FormContainer>
                 <Box component="div">
-                    <TextField label="Nome" sx={{width: '100%', paddingBottom: "16px" }}/>
-                    <TextField disabled={true} label="Descrição" sx={{width: '100%', paddingBottom: "16px" }}/>
-                    <TextField disabled={true} label="Valor" sx={{width: '100%', paddingBottom: "16px" }}/>
+                    <TextField 
+                        label="Nome" 
+                        sx={inputStyle}
+                        value={newName}
+                        onChange={(e: React.ChangeEvent<HTMLInputElement>) => 
+                            setNewName(e.target.value)}
+                    />
+                    <TextField 
+                        disabled={true} 
+                        label="Descrição" 
+                        sx={inputStyle}
+                        value={state.description}
+                    />
+                    <TextField 
+                        disabled={true} 
+                        label="Valor" 
+                        sx={inputStyle}
+                        value={state.amount}
+                    />
                     <SelectBalance />
                 </Box>
-                <FormButtons />
+                <FormButtons 
+                    path="/pagamentos"
+                    textButton="salvar"
+                    onClick={handleUpdatePaymentName} 
+                />
             </FormContainer>
         </Box>
     )
