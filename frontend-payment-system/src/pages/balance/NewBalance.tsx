9569a48c61020e2403a69f570417f59483d3382c
unknown <oseiasc2@gmail.com>
2024-01-09 17:11:41 -0300

    feat(NewBalance): implemented handleCreateBalance function

--- frontend-payment-system/src/pages/balance/NewBalance.tsx
+++ frontend-payment-system/src/pages/balance/NewBalance.tsx
@@ -1,22 +1,65 @@
 import { Box, TextField } from "@mui/material";
 import Title from "../../components/Title";
 import FormContainer from "../../components/FormContainer";
-import SelectBalance from "../../components/SelectBalance";
 import FormButtons from "../../components/FormButtons";
+import { httpClient } from "../../utils/http";
+import React, { useState } from "react";
+import { inputStyle } from "../../styles/global.style";
+import { useDispatch } from "react-redux";
+import { createBalance } from "../../redux/balance.slice";
+import { useNavigate } from "react-router-dom";
 
 export default function NewBalance(){
+    const [ balance, setBalance ] = useState(initialBalance)
+    const dispatch = useDispatch()
+    const navigate = useNavigate()
+
+    const handleCreateBalance = () => {
+        httpClient("balance", "POST", balance)
+            .then((res) => dispatch(createBalance(res.data)))
+            return navigate("/saldos")
+    }
+
     return(
         <Box component="section">
             <Title title="Criar novo saldo" />
             <FormContainer>
                 <Box component="div">
-                    <TextField label="Nome" sx={{width: '100%', paddingBottom: "16px" }}/>
-                    <TextField label="Descrição" sx={{width: '100%', paddingBottom: "16px" }}/>
-                    <TextField label="Valor" sx={{width: '100%', paddingBottom: "16px" }}/>
-                    <SelectBalance />
+                    <TextField 
+                        label="Nome" 
+                        sx={inputStyle}
+                        onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+                            setBalance({...balance, balanceName: e.target.value})
+                        }}
+                    />
+                    <TextField 
+                        label="Descrição" 
+                        sx={inputStyle}
+                        onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+                            setBalance({...balance, description: e.target.value})
+                        }}
+                    />
+                    <TextField 
+                        label="Valor" 
+                        sx={inputStyle}
+                        type="number"
+                        onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+                            setBalance({...balance, amount: Number(e.target.value)})
+                        }}
+                    />
                 </Box>
-                <FormButtons />
+                <FormButtons 
+                    path="/saldos" 
+                    textButton="criar"
+                    onClick={handleCreateBalance}
+                />
             </FormContainer>
         </Box>
     )
+}
+
+const initialBalance = {
+    balanceName: "",
+    description: "",
+    amount: 0,
 }
