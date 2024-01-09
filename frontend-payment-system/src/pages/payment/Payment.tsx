f091a3ed2a161931350be75e92d063dc449bcc03
Oséias Costa <oseiasc2@gmail.com>
2024-01-08 23:47:56 -0300

    fix(Payment): fixed pass props to payment table and fixed path to empty table button

--- frontend-payment-system/src/pages/payment/Payment.tsx
+++ frontend-payment-system/src/pages/payment/Payment.tsx
@@ -3,26 +3,27 @@ import Title from "../../components/Title";
 import EmptyTable from "../../components/EmptyTable";
 import PaymentTable from "../../components/PaymentTable";
 import TitleWithButton from "../../components/TitleWithButton";
+import { useGetPayments } from "../../hooks/useGetPayments";
 
 export default function Payment(){
-    const data = false 
+    const data = useGetPayments()
 
-    if(data) {
+    if(!data) {
         return(
             <Box component="section">
                 <Title title="Pagamentos" />
                 <EmptyTable 
                     text="Você não possui pedidos abertos" 
                     buttonText="Criar Pedido"
-                    path="/entrar"
+                    path="/pagamentos/criar"
                 />
             </Box>
         )
-    } else {
-        return (
-        <>
-            <TitleWithButton path="/pagamentos/criar" title="Pagamentos" />
-            <PaymentTable />
-        </>)
     }
+        return (
+            <Box component="section">
+                <TitleWithButton path="/pagamentos/criar" title="Pagamentos" />
+                <PaymentTable rows={data.payment} />
+            </Box>
+        )
 }
