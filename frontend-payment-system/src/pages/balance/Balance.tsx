37ad98665bc2c8800039df151af09f56ffa0428d
Oséias Costa <oseiasc2@gmail.com>
2024-01-09 10:42:51 -0300

    fix(Balance.tsx): pass props data to BalanceTable

--- frontend-payment-system/src/pages/balance/Balance.tsx
+++ frontend-payment-system/src/pages/balance/Balance.tsx
@@ -1,29 +1,29 @@
 import { Box } from "@mui/material";
 import Title from "../../components/Title";
 import EmptyTable from "../../components/EmptyTable";
-import PaymentTable from "../../components/PaymentTable";
 import TitleWithButton from "../../components/TitleWithButton";
 import BalanceTable from "../../components/BalanceTable";
+import { useGetBalance } from "../../hooks/useGetBalance";
 
 export default function Balance(){
-    const data = false 
+    const data = useGetBalance() 
 
-    if(data) {
+    if(!data) {
         return(
             <Box component="section">
                 <Title title="Saldos" />
                 <EmptyTable 
                     text="Você não possui saldos" 
                     buttonText="Criar Saldo"
-                    path="saldos/criar"
+                    path="/saldos/criar"
                 />
             </Box>
         )
-    } else {
+    } 
         return (
-        <>
-            <TitleWithButton path="/saldos/criar" title="Saldos" />
-            <BalanceTable />
-        </>)
-    }
+            <Box component="section">
+                <TitleWithButton path="/saldos/criar" title="Saldos" />
+                <BalanceTable balance={data} />
+            </Box>
+        )
 }
