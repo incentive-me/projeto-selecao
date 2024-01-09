32f1eb391e6f352289ae4271eae636e35dc9ae52
Oséias Costa <oseiasc2@gmail.com>
2024-01-09 12:16:46 -0300

    fix(useGetPayments): included usestate

--- frontend-payment-system/src/hooks/useGetPayments.ts
+++ frontend-payment-system/src/hooks/useGetPayments.ts
@@ -1,16 +1,19 @@
 import { useDispatch, useSelector } from "react-redux";
 import { RootState } from "../redux/store";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { httpClient } from "../utils/http";
-import { fetchPayments } from "../redux/payment.slice";
+import { Payment, fetchPayments } from "../redux/payment.slice";
 
 export function useGetPayments(){
     const dispatch = useDispatch()
-    const data = useSelector((state: RootState) => state.payment)
+    const [data, setData] = useState()
 
     useEffect(() => {
         httpClient("payment", "get", {})
-            .then((res) => dispatch(fetchPayments(res.data)))
+            .then((res) => {
+                dispatch(fetchPayments(res.data))
+                setData(res.data)
+            })
     },[])
 
     return data
