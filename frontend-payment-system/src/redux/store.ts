45d34f3622f83f02e91e5b5f18277cf85d784020
Oséias Costa <oseiasc2@gmail.com>
2024-01-09 08:57:40 -0300

    feat(store.tsx): included balanceSlice to store

--- frontend-payment-system/src/redux/store.ts
+++ frontend-payment-system/src/redux/store.ts
@@ -1,11 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
 import userSlice from "./user.slice";
 import paymentSlice from "./payment.slice";
+import balanceSlice from "./balance.slice";
 
 export const store = configureStore({
     reducer: {
         user: userSlice,
-        payment: paymentSlice
+        payment: paymentSlice,
+        balance: balanceSlice
     }})
 
 export type RootState = ReturnType<typeof store.getState>;
