b115f537fdd8b31d9f1226f2c6bae73c882635b5
Oséias Costa <oseiasc2@gmail.com>
2024-01-10 17:56:30 -0300

    feat(NewBalance): implement errors message on inputs

--- frontend-payment-system/src/pages/balance/NewBalance.tsx
+++ frontend-payment-system/src/pages/balance/NewBalance.tsx
@@ -8,28 +8,44 @@ import { inputStyle } from "../../styles/global.style";
 import { useDispatch } from "react-redux";
 import { createBalance } from "../../redux/balance.slice";
 import { useNavigate } from "react-router-dom";
+import { errorBalanceMessage } from "../../utils/balanceErrorMessages";
 
 export default function NewBalance(){
     const [ balance, setBalance ] = useState(initialBalance)
     const dispatch = useDispatch()
     const navigate = useNavigate()
+    const [err, setErr] = useState<ErrorMessage>(initialStateErrMessage)
 
     const handleCreateBalance = () => {
         httpClient("balance", "POST", balance)
-            .then((res) => dispatch(createBalance(res.data)))
-            return navigate("/saldos")
+            .then((res) => {
+                dispatch(createBalance(res.data))
+                return navigate("/saldos", {
+                    state: {
+                        message: "Saldo criado com sucesso",
+                        open: true
+                    }
+                })
+            }).catch((err) => {
+                const error = err.response.data.error
+                setErr(errorBalanceMessage(error))
+            })
     }
 
     return(
+        <>
         <Box component="section">
             <Title title="Criar novo saldo" />
             <FormContainer>
                 <Box component="div">
                     <TextField 
                         label="Nome" 
+                        error={err.field === "name"}
+                        helperText={err.field === "name" && err.message}
                         sx={inputStyle}
                         onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
                             setBalance({...balance, balanceName: e.target.value})
+                            setErr(initialStateErrMessage)
                         }}
                     />
                     <TextField 
@@ -43,8 +59,11 @@ export default function NewBalance(){
                         label="Valor" 
                         sx={inputStyle}
                         type="number"
+                        error={err.field === "amount"}
+                        helperText={err.field === "amount" && err.message}
                         onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
                             setBalance({...balance, amount: Number(e.target.value)})
+                            setErr(initialStateErrMessage)
                         }}
                     />
                 </Box>
@@ -55,6 +74,7 @@ export default function NewBalance(){
                 />
             </FormContainer>
         </Box>
+        </>
     )
 }
 
@@ -62,4 +82,14 @@ const initialBalance = {
     balanceName: "",
     description: "",
     amount: 0,
+}
+
+export type ErrorMessage = {
+    field: string, 
+    message: string
+}
+
+export const initialStateErrMessage = {
+    field: "", 
+    message: ""
 }
