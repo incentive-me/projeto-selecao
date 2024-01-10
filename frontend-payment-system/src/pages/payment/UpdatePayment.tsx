600e60813e3c13429545e6a6569c8e58c7ea7ef8
Oséias Costa <oseiasc2@gmail.com>
2024-01-10 15:12:56 -0300

    fix(UpdatePayment): fixed disable select balance

--- frontend-payment-system/src/pages/payment/UpdatePayment.tsx
+++ frontend-payment-system/src/pages/payment/UpdatePayment.tsx
@@ -1,20 +1,22 @@
-import { Box, TextField } from "@mui/material";
+import { Box, FormControl, InputLabel, MenuItem, Select, TextField } from "@mui/material";
 import Title from "../../components/Title";
 import FormContainer from "../../components/FormContainer";
-import SelectBalance from "../../components/SelectBalance";
 import FormButtons from "../../components/FormButtons";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { useLocation, useNavigate } from "react-router-dom";
 import { useState } from "react";
 import { inputStyle } from "../../styles/global.style";
 import { httpClient } from "../../utils/http";
 import { updatePaymentName } from "../../redux/payment.slice";
+import { RootState } from "../../redux/store";
 
 export default function UpdatePayment(){
     const dispatch = useDispatch()
     const navigate = useNavigate()
     const { state } = useLocation()
     const [ newName, setNewName ] = useState(state.name)
+    const balance = useSelector((state:RootState) => state.balance)
+    const findBalance = balance.balance.filter((item) => item.id === state.balanceAccount)
 
     const handleUpdatePaymentName = () => {
         httpClient("payment", "PATCH", {payment: state, newName})
@@ -47,7 +49,17 @@ export default function UpdatePayment(){
                         sx={inputStyle}
                         value={state.amount}
                     />
-                    <SelectBalance />
+                    <FormControl sx={{minWidth: "100%" }} disabled>
+                        <InputLabel id="demo-select-small-label">Saldo utilizado</InputLabel>
+                        <Select
+                            value={"item"}
+                            label="Saldo utilizado"
+                        >
+                            <MenuItem value={"item"}>
+                                {findBalance[0].balanceName} - R$ {findBalance[0].totalValue.toFixed(2)}
+                            </MenuItem>
+                        </Select>
+                    </FormControl>
                 </Box>
                 <FormButtons 
                     path="/pagamentos"
