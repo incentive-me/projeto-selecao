b2c0605b0cce3f2d32dd31e2a2691b6869f57ae8
Oséias Costa <oseiasc2@gmail.com>
2024-01-10 17:08:46 -0300

    feat(Payment): send notification case create payment

--- frontend-payment-system/src/pages/payment/Payment.tsx
+++ frontend-payment-system/src/pages/payment/Payment.tsx
@@ -4,9 +4,14 @@ import EmptyTable from "../../components/EmptyTable";
 import PaymentTable from "../../components/PaymentTable";
 import TitleWithButton from "../../components/TitleWithButton";
 import { useGetPayments } from "../../hooks/useGetPayments";
+import { useLocation } from "react-router-dom";
+import { useState } from "react";
+import Notification from "../../components/Notification";
 
 export default function Payment(){
     const data = useGetPayments()
+    const { state } = useLocation()
+    const [openNotification, setOpenNotification ] = useState(state?.open)
 
     if(!data) {
         return(
@@ -21,9 +26,16 @@ export default function Payment(){
         )
     }
         return (
+            <>
             <Box component="section">
                 <TitleWithButton path="/pagamentos/criar" title="Pagamentos" />
                 <PaymentTable payment={data} />
             </Box>
+            <Notification 
+                open={openNotification} 
+                setOpen={setOpenNotification}
+                message={state?.message} 
+            />
+            </>
         )
 }
