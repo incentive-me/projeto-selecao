e7b4d6054778b4548fde772740266b362f0465e7
Oséias Costa <oseiasc2@gmail.com>
2024-01-10 17:07:40 -0300

    feat(Balance): send notification if status ok

--- frontend-payment-system/src/pages/balance/Balance.tsx
+++ frontend-payment-system/src/pages/balance/Balance.tsx
@@ -4,9 +4,14 @@ import EmptyTable from "../../components/EmptyTable";
 import TitleWithButton from "../../components/TitleWithButton";
 import BalanceTable from "../../components/BalanceTable";
 import { useGetBalance } from "../../hooks/useGetBalance";
+import Notification from "../../components/Notification";
+import { useState } from "react";
+import { useLocation } from "react-router-dom";
 
 export default function Balance(){
-    const data = useGetBalance() 
+    const data = useGetBalance()
+    const { state } = useLocation()
+    const [openNotification, setOpenNotification ] = useState(state?.open)
 
     if(!data) {
         return(
@@ -21,9 +26,16 @@ export default function Balance(){
         )
     } 
         return (
+            <>
             <Box component="section">
                 <TitleWithButton path="/saldos/criar" title="Saldos" />
                 <BalanceTable rows={data} />
             </Box>
+            <Notification 
+                open={openNotification} 
+                setOpen={setOpenNotification}
+                message={state?.message} 
+            />
+            </>
         )
 }
