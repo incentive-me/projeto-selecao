f5b6fb8906cdc806500bee5e1a062ea19748f1b5
Oséias Costa <oseiasc2@gmail.com>
2024-01-10 14:45:05 -0300

    feat(App.tsx): included Load component and verify token

--- frontend-payment-system/src/App.tsx
+++ frontend-payment-system/src/App.tsx
@@ -1,10 +1,27 @@
-import { useEffect } from "react";
-
+import { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { RootState } from "./redux/store";
+import { httpClient } from "./utils/http";
+import { fecthUser } from "./redux/user.slice";
+import Load from "./components/Load";
 
 export default function App() {
-  const getToken = localStorage.getItem("paymentsToken")
+  const [load, setLoad] = useState(true)
+  const dispatch = useDispatch()
+  const token = localStorage.getItem("paymentsToken")
+  const user = useSelector((user: RootState) => user.user.user)
 
-  return (
-    <></>
-  );
+  useEffect(() => {
+      if(user.id === "" && token){
+          httpClient("user", "GET", {})
+          .then((res) => 
+            dispatch(fecthUser(res.data[0])))
+          .catch(() => localStorage.removeItem("paymentsToken"))
+          setLoad(false)
+      } else {
+        setLoad(false)
+    }
+  }, [])
+
+  return <Load load={load} />
 }
