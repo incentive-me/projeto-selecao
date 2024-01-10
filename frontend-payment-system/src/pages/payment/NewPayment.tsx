f5aeda860749b148c689e338ab708ef8485167df
Oséias Costa <oseiasc2@gmail.com>
2024-01-09 23:14:08 -0300

    feat(NewPayment): implemented function handleCreatePayment

--- frontend-payment-system/src/pages/payment/NewPayment.tsx
+++ frontend-payment-system/src/pages/payment/NewPayment.tsx
@@ -5,13 +5,38 @@ import SelectBalance from "../../components/SelectBalance";
 import FormButtons from "../../components/FormButtons";
 import { inputStyle } from "../../styles/global.style";
 import React, { useState } from "react";
+import { httpClient } from "../../utils/http";
+import { useDispatch } from "react-redux";
+import { createPayment } from "../../redux/payment.slice";
+import { decreaseBalance } from "../../redux/balance.slice";
+import { useNavigate } from "react-router-dom";
 
 export default function NewPayment(){
+    const dispatch = useDispatch()
+    const navigate = useNavigate()
+    const [balanceAccount, setBalanceAccount] = useState("")
     const [newPayment, setNewPayment] = useState({
         name: "",
         description: "",
         amount: ""
     })
+    const decreaseB = { balanceAccount, amount: newPayment.amount}
+
+    const handleCreatePayment = () => {
+        httpClient("payment", "POST", {
+            payment: {
+                name: newPayment.name, 
+                description: newPayment.description,
+                amount: newPayment.amount,
+                balanceAccount: balanceAccount
+            } 
+        }).then(res => {
+            dispatch(createPayment(res.data))
+            dispatch(decreaseBalance(decreaseB))
+            navigate("/pagamentos")
+        })
+    }
+
     return(
         <Box component="section">
             <Title title="Criar pedido de pagamento" />
@@ -35,9 +60,13 @@ export default function NewPayment(){
                         onChange={(e: React.ChangeEvent<HTMLInputElement>) => 
                             setNewPayment({...newPayment, amount: e.target.value})}
                     />
-                    <SelectBalance />
+                    <SelectBalance setBalance={setBalanceAccount} />
                 </Box>
-                <FormButtons />
+                <FormButtons 
+                    path="/pagamentos" 
+                    textButton="criar"
+                    onClick={handleCreatePayment}
+                />
             </FormContainer>
         </Box>
     )
