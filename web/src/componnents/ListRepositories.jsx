3c20fa06ea492ad17695cf23d5f29cb1cd955863
David Felipe <felipe30.10@outlook.com>
2024-01-10 11:38:51 -0300

    feat: create componnent loading

--- web/src/componnents/ListRepositories.jsx
+++ web/src/componnents/ListRepositories.jsx
@@ -2,10 +2,13 @@ import { useEffect, useMemo, useState } from "react";
 import { Repo } from "./Repo";
 import axios from "axios";
 import { FilterTag } from "./FilterTag";
+import { Loading } from "./Loading"
 
 export function ListRepositories({ user }){
+
     const [repositories, setRepositories] = useState([])
     const [tag, setTag] = useState([])
+    const [isLoding, setIsLoding] = useState(true)
 
     async function getAllTags() {
         const response = await axios.get(`http://localhost:3000/tags`)
@@ -18,11 +21,20 @@ export function ListRepositories({ user }){
     async function getAllRepositories() {
         const response = await axios.get(`http://localhost:3000/${user.id}`)
         setRepositories(response.data)
+        setIsLoding(false)
     }
 
     useMemo(() => {
         getAllRepositories()
     },[user])
+
+    if(isLoding){
+        return (
+            <div className="text-zinc-200 w-full flex justify-center items-center">
+                <Loading/>
+            </div>
+        )
+    }
 
 
     return (
