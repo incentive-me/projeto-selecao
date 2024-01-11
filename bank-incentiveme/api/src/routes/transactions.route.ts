46ac055e96f404d31da4f804a427ac4c4d0ea126
Adrian Braga <adrian.braga.ro@gmail.com>
2024-01-11 00:48:34 -0400

    feat: CRUD route transactions

--- bank-incentiveme/api/src/routes/transactions.route.ts
+++ bank-incentiveme/api/src/routes/transactions.route.ts
@@ -7,7 +7,7 @@ const prisma = new PrismaClient();
 
 export async function transactionsRoute(app: FastifyInstance) {
   app.post(
-    '/create', 
+    '/create',
     {
       preHandler: [ensureAuthenticated]
     },
@@ -21,7 +21,7 @@ export async function transactionsRoute(app: FastifyInstance) {
         price: z.number()
       });
 
-      const { 
+      const {
         description,
         type,
         category,
@@ -39,5 +39,87 @@ export async function transactionsRoute(app: FastifyInstance) {
       });
 
       return reply.status(201).send();
+    });
+
+  app.get(
+    '/',
+    {
+      preHandler: [ensureAuthenticated]
+    },
+    async (request) => {
+      const { user } = request;
+      const listTransactions = await prisma.transaction.findMany({
+        where: {
+          user_id: user.id
+        }
+      });
+
+      return {
+        listTransactions
+      };
+    });
+
+  app.put(
+    '/edit/:id',
+    {
+      preHandler: [ensureAuthenticated]
+    },
+    async (request, reply) => {
+      const transactionParams = z.object({
+        id: z.string().uuid()
+      });
+
+      const { user } = request;
+      const { id } = transactionParams.parse(request.params);
+
+      const createTransactionBodySchema = z.object({
+        description: z.string(),
+        type: z.enum(['income', 'outcome']),
+        category: z.string(),
+        price: z.number()
+      });
+
+      const {
+        description,
+        type,
+        category,
+        price
+      } = createTransactionBodySchema.parse(request.body);
+
+      await prisma.transaction.update({
+        where: {
+          id: id
+        },
+        data: {
+          description,
+          type,
+          category,
+          price,
+          user_id: user.id
+        }
+      });
+
+      return reply.status(201).send();
+    });
+
+  app.delete(
+    '/delete/:id',
+    {
+      preHandler: [ensureAuthenticated]
+    },
+    async (request, reply) => {
+      const transactionParams = z.object({
+        id: z.string().uuid()
+      });
+
+      const { id } = transactionParams.parse(request.params);
+
+      const listTransactions = await prisma.transaction.delete({
+        where: {
+          id: id
+        }
+      });
+
+      return reply.status(200).send();
     });
 }
