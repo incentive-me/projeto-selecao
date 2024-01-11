482a0929c893132991f76c558b476c35e38130de
Oséias Costa <oseiasc2@gmail.com>
2024-01-11 10:52:28 -0300

    fixed(Payment.tsx): inluded Notification component again

--- frontend-payment-system/src/pages/payment/Payment.tsx
+++ frontend-payment-system/src/pages/payment/Payment.tsx
@@ -4,9 +4,14 @@ import EmptyTable from "../../components/EmptyTable";
 import PaymentTable from "../../components/PaymentTable";
 import TitleWithButton from "../../components/TitleWithButton";
 import { useGetPayments } from "../../hooks/useGetPayments";
+import { useLocation } from "react-router-dom";
+import { useState } from "react";
+import Notification from "../../components/Notification";
 
 export default function Payment(){
     const data = useGetPayments()
+    const { state } = useLocation()
+    const [openNotification, setOpenNotification ] = useState(state?.open)
 
     if(data.length === 0) {
         return(
@@ -26,6 +31,12 @@ export default function Payment(){
                 <TitleWithButton path="/pagamentos/criar" title="Pagamentos" />
                 <PaymentTable payment={data} />
             </Box>
+            <Notification 
+                open={openNotification} 
+                setOpen={setOpenNotification}
+                message={state?.message}
+                type={state.type} 
+            />
             </>
         )
 }
