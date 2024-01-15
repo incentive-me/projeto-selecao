8fb58ed96d3fc404abfbaf1962aef2b1312dca66
Darlan Bomfim <darlanbs05@gmail.com>
2024-01-15 15:13:29 -0300

    :sparkles: Feat: componetizando icones de logout,pagamento e carteira

---
+++ src/components/IconsComponents/LogOutIcon.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+const LogoutIcon = () => {
+  return (
+    <img src='/icons/LogoutFilled.svg' width={56} height={24} alt='Icone de pagamento' style={{color: '#BDBDBD',  padding: '0px, 32px, 0px, 0px'}}/>
+  )
+}
+
+export default LogoutIcon
