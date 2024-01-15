efef0d99303dd6225d69b135c5f42a7ccb0bd764
Darlan Bomfim <darlanbs05@gmail.com>
2024-01-15 13:02:22 -0300

    Feat: Icone de usuario + componente do icone

---
+++ src/components/IconsComponents/PersonIcon.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+const PersonIcon = () => {
+  return (
+    <img src='/icons/PersonFilled.svg' width={40} height={40} alt='Icone de pessoa' style={{backgroundColor: '#BDBDBD', borderRadius: '64px', padding: '8px'}}/>
+  )
+}
+
+export default PersonIcon
