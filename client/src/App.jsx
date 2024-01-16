be0431d74260f45d38d51d4cbbfd497335847f70
felipesousac <claudioflsousa@gmail.com>
2024-01-15 21:19:01 -0300

    Adiciona diretorios com os arquivos de configuracoes iniciais

---
+++ client/src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <>
+      <div>Teste</div>
+    </>
+  );
+}
+
+export default App;
---
+++ server/src/main/java/com/projeto/selecao/SelecaoApplication.java
@@ -0,0 +1,13 @@
+package com.projeto.selecao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SelecaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SelecaoApplication.class, args);
+	}
+
+}
