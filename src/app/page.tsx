8a38cc7018398906772650855a8c48b96eb0d058
Darlan Bomfim <darlanbs05@gmail.com>
2024-01-18 19:05:55 -0300

    :sparkles: Feat: pagina home servindo como pagina de transações com validaçao de usuario logado

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,23 @@
-
+"use client";
+import Sidebar from "@/components/SideBar/Index";
+import FormComponent from "@/components/FormComponents/Index";
+import Header from "@/components/Header/Index";
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
 
 export default function Home() {
+  const router = useRouter();
+  useEffect(() => {
+    const tokenFromLocalStorage = localStorage.getItem("token");
+
+    const isTokenValid = !!tokenFromLocalStorage;
+
+    isTokenValid ? router.push("/") : router.push("/auth");
+  }, [router]);
   return (
-   <div>Home</div>
-  )
+    <>
+      <Header />
+      <Sidebar />
+    </>
+  );
 }
