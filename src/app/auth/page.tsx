51cca2f35fb7d45631372434966de61928e8b353
Darlan Bomfim <darlanbs05@gmail.com>
2024-01-18 19:05:06 -0300

    :sparkles: Feat:Criação de pagina de autenticação com validaçao de usuario logado

---
+++ src/app/auth/page.tsx
@@ -0,0 +1,12 @@
+import FormComponent from "@/components/FormComponents/Index";
+import React from "react";
+
+const SigninRegister = () => {
+  return (
+    <div>
+      <FormComponent />
+    </div>
+  );
+};
+
+export default SigninRegister;
