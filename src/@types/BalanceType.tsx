fd08a44c329d4bd03a9c114c2d99dfd0ce4c79e9
Darlan Bomfim <darlanbs05@gmail.com>
2024-01-19 15:49:07 -0300

    :recycle: alteração no Type dos saldos

--- src/@types/BalanceType.tsx
+++ src/@types/BalanceType.tsx
@@ -1,9 +1,12 @@
-export type BalanceValues = {
-  id: number;
+export type createBalanceValues = {
   nome: string;
   descricao: string;
   valor_inicial: number;
+};
+
+export type BalanceValues = {
+  id: number;
   valor_utilizado: number;
   valor_restante: number;
   usuario_id: number;
-};
+} & createBalanceValues;
