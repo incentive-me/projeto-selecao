0d1288bb920a0a06aaa571946367be5adab0e5a9
Darlan Bomfim <darlanbs05@gmail.com>
2024-01-21 11:15:03 -0300

    :sparkles: Feat: Requisição na API para criação de pagamento

---
+++ src/services/PaymentFetch.ts
@@ -0,0 +1,84 @@
+import axios, { AxiosError } from "axios";
+const url = "https://tan-wide-eyed-llama.cyclic.app/payment/";
+
+interface PaymentData {
+  nome: string;
+  descricao: string;
+  valor: number;
+}
+
+const id = localStorage.getItem("id");
+
+export const getPaymentPerPerson = async () => {
+  try {
+    const response = await axios.get(url + id, {
+      headers: {
+        Authorization: `Bearer ${localStorage.getItem("token")}`,
+      },
+    });
+    if (!response.data) {
+      throw new Error(`Erro na requisição: ${response.status}`);
+    }
+    return response.data;
+  } catch (error: any) {
+    if (axios.isAxiosError(error)) {
+      throw new Error(`Erro ao buscar saldos: ${error.message}`);
+    }
+  }
+};
+
+export const createPayment = async (
+  balanceId: string,
+  nome: string,
+  descricao: string,
+  valor: number
+): Promise<PaymentData> => {
+  try {
+    const response = await axios.post(
+      url + "payment/" + id + "/?balanceId=" + id,
+      {
+        nome,
+        descricao,
+        valor,
+      },
+      {
+        headers: {
+          Authorization: `Bearer ${localStorage.getItem("token")}`,
+        },
+      }
+    );
+
+    if (!response.data) {
+      throw new Error(`Erro na requisição: ${response.status}`);
+    }
+
+    return response.data;
+  } catch (error: any) {
+    if (axios.isAxiosError(error)) {
+      throw new Error(`Erro ao criar saldo: ${error.message}`);
+    }
+
+    throw new Error(`Erro interno no servidor`);
+  }
+};
+
+export const deletePaymentByBalanceId = async (balanceId: string) => {
+  try {
+    const response = await axios.delete(
+      url + "payment/" + id + "/?balanceId=" + balanceId,
+      {
+        headers: {
+          Authorization: `Bearer ${localStorage.getItem("token")}`,
+        },
+      }
+    );
+    if (!response.data) {
+      throw new Error(`Erro na requisição: ${response.status}`);
+    }
+    return response.data;
+  } catch (error: any) {
+    if (axios.isAxiosError(error)) {
+      throw new Error(`Erro ao deletar saldo: ${error.message}`);
+    }
+  }
+};
