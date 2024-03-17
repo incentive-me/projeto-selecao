ee2a220668fd3699134af2225354f3ca082b2d0f
nicolasmoovin <nicolas.jardin@moovin.com.br>
2024-03-17 16:10:36 -0300

    refactor: Remover arquivos nao utilizados

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,6 @@
 import { Module } from '@nestjs/common';
 import { APP_GUARD } from '@nestjs/core';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+
 import { AuthModule } from './auth/auth.module';
 import { JwtAuthGuard } from './auth/guards/jwt-auth.guard';
 import { PrismaModule } from './prisma/prisma.module';
@@ -9,9 +8,7 @@ import { UserModule } from './user/user.module';
 
 @Module({
   imports: [PrismaModule, UserModule, AuthModule],
-  controllers: [AppController],
   providers: [
-    AppService,
     {
       provide: APP_GUARD,
       useClass: JwtAuthGuard,
